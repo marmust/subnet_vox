9df68625f35315b23c26d7c51585dac2ff5e05ff
me after unity <daniel.varkin@gmail.com>
2025-02-25 23:44:00 +0200

    Initial commit

---
+++ build/CMakeFiles/subnet_vox.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subnet_vox.dir/main.cpp.obj"
+  "CMakeFiles/subnet_vox.dir/main.cpp.obj.d"
+  "libsubnet_vox.dll.a"
+  "subnet_vox.exe"
+  "subnet_vox.exe.manifest"
+  "subnet_vox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subnet_vox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "asio/include/asio.hpp"
+
+int main(int, char**){
+    std::cout << "Hello, from subnet_vox!\n";
+}
