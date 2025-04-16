e2958443e545e231485cd4bd1a07814250f870f0
me after unity <daniel.varkin@gmail.com>
2025-04-16 15:07:25 +0300

    added readme

---
+++ build_linux/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sedgrz/Desktop/subnet_vox/client.cpp" "CMakeFiles/client.dir/client.cpp.o" "gcc" "CMakeFiles/client.dir/client.cpp.o.d"
+  "/home/sedgrz/Desktop/subnet_vox/consoleGraphics.cpp" "CMakeFiles/client.dir/consoleGraphics.cpp.o" "gcc" "CMakeFiles/client.dir/consoleGraphics.cpp.o.d"
+  "/home/sedgrz/Desktop/subnet_vox/message.cpp" "CMakeFiles/client.dir/message.cpp.o" "gcc" "CMakeFiles/client.dir/message.cpp.o.d"
+  "/home/sedgrz/Desktop/subnet_vox/networking.cpp" "CMakeFiles/client.dir/networking.cpp.o" "gcc" "CMakeFiles/client.dir/networking.cpp.o.d"
+  "/home/sedgrz/Desktop/subnet_vox/userInterface.cpp" "CMakeFiles/client.dir/userInterface.cpp.o" "gcc" "CMakeFiles/client.dir/userInterface.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_linux/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/client.cpp.o"
+  "CMakeFiles/client.dir/client.cpp.o.d"
+  "CMakeFiles/client.dir/consoleGraphics.cpp.o"
+  "CMakeFiles/client.dir/consoleGraphics.cpp.o.d"
+  "CMakeFiles/client.dir/message.cpp.o"
+  "CMakeFiles/client.dir/message.cpp.o.d"
+  "CMakeFiles/client.dir/networking.cpp.o"
+  "CMakeFiles/client.dir/networking.cpp.o.d"
+  "CMakeFiles/client.dir/userInterface.cpp.o"
+  "CMakeFiles/client.dir/userInterface.cpp.o.d"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
